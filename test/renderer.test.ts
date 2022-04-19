fe2a0427e0fa6e0e27635a70c48a9baaf9ec0fa0
Daniel Roe <daniel@roe.dev>
2022-04-19 10:40:30 +0100

    fix: add `defer` if not module

--- test/renderer.test.ts
+++ test/renderer.test.ts
@@ -62,9 +62,9 @@ describe('renderer with legacy manifest', () => {
     const result = renderScripts().split('</script>').slice(0, -1).map(s => `${s}</script>`).sort()
     expect(result).to.deep.equal(
       [
-        '<script src="/_nuxt/app.js"></script>',
-        '<script src="/_nuxt/commons/app.js"></script>',
-        '<script src="/_nuxt/runtime.js"></script>'
+        '<script src="/_nuxt/app.js" defer></script>',
+        '<script src="/_nuxt/commons/app.js" defer></script>',
+        '<script src="/_nuxt/runtime.js" defer></script>'
       ]
     )
   })
